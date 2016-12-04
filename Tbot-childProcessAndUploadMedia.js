96b38fd1c589ffe9c9e4d7dfb3f45599a387ce01
pleycpl <h4rvey@riseup.net>
2016-12-04 18:17:22 +0300

    Created Tbot-childProcess and Upload Media.js

---
+++ Tbot-childProcessAndUploadMedia.js
@@ -0,0 +1,39 @@
+console.log('The bot is starting');
+var Twit = require('twit');
+var config = require('./config');
+var fs = require('fs');
+// Making a Twit object for connection to the API
+var T = new Twit(config);
+// Require child process for triggerring script for Process
+var exec = require('child_process').exec;
+
+tweetIt();
+
+function tweetIt() {
+	cmd = "convert ~/Pictures/random_picture.jpg -resize 200x200 ~/Pictures/little.jpg";
+	exec(cmd, processing);
+	function processing() {
+		var filename = "/home/chuck/Pictures/little.jpg";
+		var params = {
+			encoding: 'base64'
+		};
+		var content = fs.readFileSync(filename, params);
+		T.post('media/upload', {media_data: content}, uploaded);
+		function uploaded(err, data, res) {
+			// This is where 1 will tweet!
+			var id = data.media_id_string;
+			var tweet = {
+				status: "#twitterbot api example pic",
+				media_ids: [id]
+			};
+			function tweeted(err, data, res) {
+				if(err) {
+					console.log('Something went wrong!');
+				} else {
+					console.log('It worked!');
+				}
+			}; // tweeted
+			T.post('statuses/update', tweet, tweeted);
+		};
+	}; // process
+}; // tweetIt
